67928424f86b37df68b74819b748e9ec60f72e17
LouisBlesse <63815688+LouisBlesse@users.noreply.github.com>
2021-10-26 14:56:55 +0200

    BDD ok

--- Bank/MainWindow.xaml.cs
+++ Bank/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Bank
+﻿using System;
+using System.Data.SQLite;
+
+namespace Bank
 {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
@@ -8,6 +11,25 @@
     public MainWindow()
     {
       InitializeComponent();
+      SQLiteConnection SQL = new SQLiteConnection("Data Source=BDD.db");
+      SQL.Open();
+
+      SQLiteCommand command = SQL.CreateCommand();
+      command.CommandText = "INSERT INTO admin (identifiant, password) values (\"1111\",\"0000\")";
+      command.ExecuteNonQuery();
+      
+      
+      
+      SQLiteCommand commandSelect = SQL.CreateCommand();
+      commandSelect.CommandText = "SELECT * FROM admin";
+
+      SQLiteDataReader reader = commandSelect.ExecuteReader();
+      while (reader.Read())
+      {
+        State.Text=reader["identifiant"].ToString();
+      }
+      
+      SQL.Close();
     }
   }
 }
