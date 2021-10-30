2cf2a32224b68d7695f1a8e6ef5c308c3673ec4e
votybe <aymeric.maillot@efrei.net>
2021-10-31 00:42:14 +0200

    connexion fenetre + appel api*2

---
+++ Bank/Exchangerate.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Bank
+{
+    
+    public class Exchangerate
+    {
+        public HttpClient client { get; set; }
+
+        public Exchangerate()
+        {
+            client = new HttpClient();
+        }
+        
+        public async Task<string> Connect()
+        {
+            string res =
+                await client.GetStringAsync(
+                    "https://v6.exchangerate-api.com/v6/21408f48185cf792dba37191/pair/EUR/GBP/50000");
+            return res;
+        }
+        
+        
+        public async Task<string> ConnectGetAllCurency()
+        {
+            string res =
+                await client.GetStringAsync(
+                    "https://v6.exchangerate-api.com/v6/21408f48185cf792dba37191/codes");
+            return res;
+        }
+        
+        public class RootRoot
+        {
+            public string result { get; set; }
+            public string documentation { get; set; }
+            public string terms_of_use { get; set; }
+            public List<List<string>> supported_codes { get; set; }
+        }
+
+        public Root transfert (string json){
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json); 
+            return myDeserializedClass;
+        }
+        
+        public RootRoot transfertCurrency (string json){
+            RootRoot myDeserializedClass = JsonConvert.DeserializeObject<RootRoot>(json); 
+            return myDeserializedClass;
+        }
+        public class Root
+        {
+            public string result { get; set; }
+            public string documentation { get; set; }
+            public string terms_of_use { get; set; }
+            public int time_last_update_unix { get; set; }
+            public string time_last_update_utc { get; set; }
+            public int time_next_update_unix { get; set; }
+            public string time_next_update_utc { get; set; }
+            public string base_code { get; set; }
+            public string target_code { get; set; }
+            public double conversion_rate { get; set; }
+            public int conversion_result { get; set; }
+        }
+ 
+
+    }
+}
--- Bank/MainWindow.xaml.cs
+++ Bank/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SQLite;
+using System.Windows;
 
 namespace Bank
 {
@@ -13,23 +14,36 @@ namespace Bank
       InitializeComponent();
       SQLiteConnection SQL = new SQLiteConnection("Data Source=BDD.db");
       SQL.Open();
-
+      
+      
+      
+      
       SQLiteCommand command = SQL.CreateCommand();
       command.CommandText = "INSERT INTO admin (identifiant, password) values (\"1111\",\"0000\")";
       command.ExecuteNonQuery();
-      
-      
-      
       SQLiteCommand commandSelect = SQL.CreateCommand();
       commandSelect.CommandText = "SELECT * FROM admin";
-
+      
       SQLiteDataReader reader = commandSelect.ExecuteReader();
       while (reader.Read())
       {
         State.Text=reader["identifiant"].ToString();
       }
-      
       SQL.Close();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      { 
+        Menu p = new Menu();
+        p.Show();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString());
+        throw;
+      }    
     }
   }
 }
