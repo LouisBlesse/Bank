eaac25db9928c53301f5a7bbb51c561812b1563f
Votybe <66923802+Votybe@users.noreply.github.com>
2021-10-30 14:05:06 +0200

    interface graphique

---
+++ Bank/CreationCompteAdmin.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bank
+{
+    /// <summary>
+    /// Logique d'interaction pour CreationCompteAdmin.xaml
+    /// </summary>
+    public partial class CreationCompteAdmin : Page
+    {
+        public CreationCompteAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void mdpAdmin_Copy1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
